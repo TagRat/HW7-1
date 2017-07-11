231c307129a0d6a1cf64a99829ed96d995332563
Bill Taggart <wt49@njit.edu>
2017-07-11 19:52:30 -0400

    Added fields to display_reuslts.php for input and display

--- display_results.php
+++ display_results.php
@@ -1,13 +1,35 @@
 <?php
     // get the data from the form
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $password = filter_input(INPUT_POST, 'password');
+    $phone = filter_input(INPUT_POST, 'phone');
 
     // get the rest of the data for the form
 
     // for the heard_from radio buttons,
     // display a value of 'Unknown' if the user doesn't select a radio button
+    $heard_from = filter_input(INPUT_POST, 'heard_from'); 
+    if ($heard_from == NULL) {
+        $heard_from = 'unknown';
+    }    
 
     // for the wants_updates check box,
+    $wants_updates = $_POST['wants_updates'];
+    if (isset($wants_updates)) {
+        $wants_updates = 'Yes';
+    }    
+    else {
+            $wants_updates = 'No';	    
+    }	    
+
+    $contact_via = filter_input(INPUT_POST, 'contact_via');
+
+    $comments = filter_input(INPUT_POST, 'comments');
+    $comments = htmlspecialchars($comments);
+    $comments = nl2br($comments, false);
+
+
+
     // display a value of 'Yes' or 'No'
 ?>
 <!DOCTYPE html>
@@ -24,22 +46,24 @@
         <span><?php echo htmlspecialchars($email); ?></span><br>
 
         <label>Password:</label>
-        <span><!-- add PHP code here--></span><br>
+        <span><?php echo htmlspecialchars($password); ?></span><br>
 
         <label>Phone Number:</label>
-        <span></span><br>
+        <span><?php echo htmlspecialchars($phone); ?></span><br>
 
         <label>Heard From:</label>
-        <span></span><br>
+        <span><?php echo htmlspecialchars($heard_from); ?></span><br>
 
         <label>Send Updates:</label>
-        <span></span><br>
+        <span><?php echo $wants_updates; ?></span><br>
 
         <label>Contact Via:</label>
-        <span></span><br><br>
+        <span><?php echo htmlspecialchars($contact_via); ?></span><br><br>
 
         <span>Comments:</span><br>
-        <span></span><br>        
+        <span><?php echo $comments; ?></span><br>
+
+
     </main>
 </body>
-</html>
+</html
